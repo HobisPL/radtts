e4033da5d752eafe6394b78fb01a27c3f2fd7025
HobisPL <95121145+HobisPL@users.noreply.github.com>
2022-09-23 22:24:45 +0200

    Update symbols.py

--- tts_text_processing/symbols.py
+++ tts_text_processing/symbols.py
@@ -52,6 +52,15 @@ def get_symbols(symbol_set):
         _letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
         _arpabet = ["@" + s for s in arpabet]
         symbols = list(_punctuation + _math + _special + _accented + _numbers + _letters) + _arpabet
+    elif symbol_set == 'polski':
+        _punctuation = '!\'",.:;? '
+        _math = ''
+        _special = '_'
+        _accented = 'áçéêëñöøćž'
+        _numbers = ''
+        _letters = 'AĄBCĆDEĘFGHIJKLŁMNŃOÓPRSŚTUWYZŹŻaąbcćdeęfghijklłmnńoóprsśtuwyzźż'
+        _arpabet = ["@" + s for s in arpabet]
+        symbols = list(_punctuation + _math + _special + _accented + _numbers + _letters) + _arpabet
     else:
         raise Exception("{} symbol set does not exist".format(symbol_set))
 
